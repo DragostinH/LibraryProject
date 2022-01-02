2234ca40e113530cd8cd9f8c986a452544ae80c5
DragostinH <dragostin.hristov@gmail.com>
2022-01-02 23:09:45 +0200

    Added an ID to each div to  see if we can find the element that way

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 let localLibrary = [];
-let dynamicLibrary = [];
-// let dynamicArrayOfBooks = Array.from(nodeListOfBooks);
-const contentSection = document.querySelector('.content-section');
-
+let contentSection = document.querySelector('.content-section');
 const newBookForm = document.querySelector('.new-book-form');
 const showFormButton = document.querySelector('.show-form-button');
 const cancelFormButton = document.querySelector('.cancel-button');
@@ -11,8 +8,6 @@ const bookTitleField = document.querySelector('#book-name');
 const bookAuthorField = document.querySelector('#book-author');
 const bookPages = document.querySelector('#book-pages');
 const formCheckbox = document.querySelector('#finished');
-
-
 
 
 // Event listeners for buttons:
@@ -25,57 +20,21 @@ cancelFormButton.addEventListener('click', () => {
     hideForm(newBookForm);
 })
 
-// Add button from the form
+// Add book button from the form
 addBookButton.addEventListener('click', () => {
     let newBook = new Book(bookTitleField.value,
         bookAuthorField.value, bookPages.value, formCheckbox.checked);
     let cardDiv = createBookCard(newBook);
-    let removeButton = cardDiv.querySelector('#remove-book-button');
-    removeButton.addEventListener('click', ()=>{
-        console.log(removeButton.textContent);
-    });
+    // localLibrary.push(cardDiv);
+
     addBookToLibrary(localLibrary, cardDiv);
     hideForm(newBookForm);
-    contentSection.appendChild(createBookCard(newBook));
+    for (const item of localLibrary) {
+        contentSection.appendChild(item);
+
+    }
 
 })
-
-
-// REMOVE BOOK button from the book card element
-// let testBook2 = new Book('Book Name 2', 'Author Any2', 400, true);
-// let testBook3 = new Book('Book Name 3', 'Author Any3', 300, false);
-// let testBook4 = new Book('Book Name 4', 'Author Any4', 600, true);
-// let testBook5 = new Book('Book Name 5', 'Author Any5', 100, false);
-
-
-// localLibrary.push(testBook1);
-// localLibrary.push(testBook2);
-// localLibrary.push(testBook3);
-// localLibrary.push(testBook4);
-// localLibrary.push(testBook5);
-
-
-// let formBook = createBookCard(new Book(bookTitleField.value
-//     , bookAuthorField.value, bookPages.value, formCheckbox.checked))
-
-// let formBook = createBookCard(testBook1)
-// console.log(bookTitleField);
-// console.log(formBook);
-// localLibrary.push(testBook1);
-// for (const book of localLibrary) {
-//     contentSection.appendChild(createBookCard(book));
-// }
-// console.table(localLibrary);
-
-
-
-
-
-
-
-
-
-
 
 
 // Function for adding book to library
@@ -84,7 +43,7 @@ function addBookToLibrary(library, book) {
 }
 
 // Function for removing item from list
-function removeBookFromArray(library, index){
+function removeBookFromArray(library, index) {
     library.splice(index, 1);
 }
 // Function to cancel form
@@ -99,8 +58,12 @@ function showForm(element) {
 
 // Function for creating the book card element
 function createBookCard(book) {
-
+    let id = Math.floor(Math.random() * 2000000) + "";
     let cardContainer = document.createElement('div');
+    cardContainer.id = id;
+
+    // bookID.style.display = 'none';
+
 
     let bookTitle = document.createElement('p');
     let bookAuthor = document.createElement('p');
@@ -112,9 +75,22 @@ function createBookCard(book) {
 
     let editButton = document.createElement('button');
     editButton.id = 'edit-book-button';
+    editButton.addEventListener('click', () => {
+        console.log('Going to add this functionality later')
+    });
 
     let removeButton = document.createElement('button');
     removeButton.id = 'remove-book-button';
+    removeButton.addEventListener('click', () => {
+       
+
+
+        // localLibrary.splice(index, 1);
+        console.log(cardContainer);
+        console.log(localLibrary);
+
+
+    })
 
     cardContainer.appendChild(bookTitle);
     cardContainer.appendChild(bookAuthor);
@@ -130,8 +106,6 @@ function createBookCard(book) {
     bookFinished.checked = book.finished;
     editButton.textContent = 'EDIT';
     removeButton.textContent = 'REMOVE BOOK';
-
-
 
     return cardContainer;
 }
